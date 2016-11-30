fd8f6609380bf6b7f3b2126a1e24a26ae484666d
David Berry <d.berry@jach.hawaii.edu>
2016-11-30 15:34:25 +0000

    The ReportLevel attribute of the Channel class is not boolean

--- channel.c
+++ channel.c
@@ -67,12 +67,12 @@ f     - AST_WRITE: Write an Object to a Channel
 *     License as published by the Free Software Foundation, either
 *     version 3 of the License, or (at your option) any later
 *     version.
-*     
+*
 *     This program is distributed in the hope that it will be useful,
 *     but WITHOUT ANY WARRANTY; without even the implied warranty of
 *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *     GNU Lesser General Public License for more details.
-*     
+*
 *     You should have received a copy of the GNU Lesser General
 *     License along with this program.  If not, see
 *     <http://www.gnu.org/licenses/>.
@@ -5247,7 +5247,7 @@ astMAKE_TEST(Channel,Indent,( this->indent != -INT_MAX ))
 *     Public attribute.
 
 *  Synopsis:
-*     Integer (boolean).
+*     Integer.
 
 *  Description:
 *     This attribute determines which, if any, of the conditions that occur
