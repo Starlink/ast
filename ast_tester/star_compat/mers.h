61a3082f8ffce006c664f555354d49f46058da12
E. Madison Bray <676149+embray@users.noreply.github.com>
2026-04-01 12:06:47 +0200

    test: testyamlchan was rewritten from Fortran to C in 20d3d7d63 ;
    account for that change following rebase ; "mers.h" is included in the
    file but I'm not exactly sure how it's used except for defines already
    in "sae_par.h", so just make a small stub for it

---
+++ ast_tester/star_compat/mers.h
@@ -0,0 +1,7 @@
+/* Minimal mers.h stub for standalone (non-Starlink) builds.
+   No mers functions are called by AST tests; this stub satisfies the
+   #include without pulling in Starlink EMS/MSG headers. */
+#ifndef MERS_DEFINED
+#define MERS_DEFINED
+#include "sae_par.h"
+#endif
